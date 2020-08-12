5d016820d5f94cf1d8c3daa7c1a5112530f62fa0
Alvin <huerox@msn.com>
2020-08-11 21:21:11 -0700

    express router.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,47 +12,27 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public')); //used to serve files from the public folder
 app.use(bodyParser.json()); //helps body-parser middleware parse JSON objects. Parses JSON requests into objects of the request body so that they can be accessed more easily
 
-app.all('/campsites', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    next();
-});
+app.use('/campsites', campsiteRouter);
 
-app.get('/campsites', (req, res) => {
-    res.end('Will send all the campsites to you');
-});
 
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`)
-});
+// app.get('/campsites/:campsiteId', (req, res) => {
+//     res.end(`We will send details of the campsite: ${req.params.campsiteId} to you`);
+// });
 
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-});
+// app.post('/campsites/:campsiteId', (req, res) => {
+//     res.statusCode = 403;
+//     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
+// });
 
-app.delete('/campsites', (req, res) => {
-    res.end('Deleting all campsites');
-});
+// app.put('/campsites/:campsiteId', (req, res) => {
+//     res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+//     res.end(`Will update the campsite: ${req.body.name}
+//     with description ${req.body.description}`)
+// });
 
-app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`We will send details of the campsite: ${req.params.campsiteId} to you`);
-});
-
-app.post('/campsites/:campsiteId', (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
-});
-
-app.put('/campsites/:campsiteId', (req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name}
-    with description ${req.body.description}`)
-});
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+// app.delete('/campsites/:campsiteId', (req, res) => {
+//     res.end(`Deleting campsite: ${req.params.campsiteId}`);
+// });
 
 app.use((req, res) => {
     //console.log(req.headers); no longer needed because morgan will take care of logging the headers
