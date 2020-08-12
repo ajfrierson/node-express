61d259856ae15f7fe810f90c852b3e911a0faabf
Alvin <huerox@msn.com>
2020-08-11 20:08:17 -0700

    installed morgan middleware to serve up static HTML files.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public')); //used to serve files from the public folder
+
 app.use((req, res) => {
-    console.log(req.headers);
+    //console.log(req.headers); no longer needed because morgan will take care of logging the headers
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an express server</h1></body></html>')
